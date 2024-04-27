bb246349f22cf3bdfffcf77fcaabe934d157ae78
emphyrea <111723204+emphyrea@users.noreply.github.com>
2024-04-27 16:23:09 -0500

    wip update gamemode
    -working on spawning both characters to control

---
+++ Source/FetchQuestProto/Private/FetchGameMode.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FetchPet.h"
+#include "MainCharacter.h"
+#include "GameFramework/GameModeBase.h"
+#include "FetchGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AFetchGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AFetchGameMode();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "PlayerRefs")
+	AFetchPet* pet;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "PlayerRefs")
+	AMainCharacter* player1;
+};
